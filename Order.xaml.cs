01f194ededc55226e487d6f2a8e79cf6ad563d2c
utto2000 <bartek200039@gmail.com>
2022-07-08 15:57:22 +0200

    Dodaj pliki projektów.

---
+++ Order.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Kawiarnia
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Order.xaml
+    /// </summary>
+    public partial class Order : Window
+    {
+        public Order()
+        {
+            InitializeComponent();
+        }
+
+        private void BackToMenu(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            this.Visibility = Visibility.Hidden;
+            mainWindow.Show();
+        }
+    }
+}
